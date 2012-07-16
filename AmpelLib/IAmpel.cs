2559abb61ed70fe92bd5d08204fb916a4bec4246
Alex <alexanderchernenko@gmail.com>
2012-07-17 00:03:11 +0300

    interface added

---
+++ AmpelLib/IAmpel.cs
@@ -0,0 +1,9 @@
+﻿namespace AmpelLib
+{
+    public interface IAmpel
+    {
+        void Light(LightColor color);
+        void Light(LightColor color1, LightColor color2);
+        void Off();
+    }
+}
