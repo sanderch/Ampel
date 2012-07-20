5062ceacd0e02532d94f9196466c003d801baaad
Alex <alexanderchernenko@gmail.com>
2012-07-20 23:22:53 +0300

    test project added

---
+++ AmpelTest/AmpelStub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AmpelLib;
+
+namespace AmpelTest
+{
+    class AmpelStub : IAmpel
+    {
+        public void Light(LightColor color)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Light(LightColor color1, LightColor color2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Off()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
