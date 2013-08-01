f9bac3586d416a612d49aea2621efd0d35d413d5
Alex Chernenko <alexanderchernenko@gmail.com>
2013-08-01 19:05:53 +0300

    Final builder implementation added

---
+++ AmpelLib/FinalBuilderProjectInfoProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AmpelLib.ConfigSections;
+using FBService;
+
+namespace AmpelLib
+{
+	public class FinalBuilderProjectInfoProvider : IProjectInfoProvider
+	{
+		private readonly IBuildServer _buildServer;
+
+		public FinalBuilderProjectInfoProvider()
+		{
+			_buildServer = new FBGateway();
+		}
+
+		public List<ProjectInformationDto> GetProjectInfos(ProviderConfigElement providerInfo)
+		{
+			return _buildServer.GetAllProjects().Select(x => new ProjectInformationDto
+				{
+					Name = x.Name,
+					Status = ParseFBStatus(x.Status)
+				}).ToList();
+		}
+
+		private static ProjectStatusEnum ParseFBStatus(ProjectStatus status)
+		{
+			ProjectStatusEnum enumRes;
+			return Enum.TryParse(status.ToString(), out enumRes) ? enumRes : ProjectStatusEnum.Failure;
+		}
+	}
+}
